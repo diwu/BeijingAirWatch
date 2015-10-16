4f1999d9f7a79a422ec2cbb6b02d79a4bd24f7d6
Di Wu <diwufet@163.com>
2015-10-16 16:54:45 +0800

    interface controller will refresh data when displayed

--- BeijingAirWatch WatchKit Extension/InterfaceController.swift
+++ BeijingAirWatch WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,11 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    private var aqi: Int = -1
+    private var concentration: Double = -1.0
+    private var time: String? = "Invalid"
+    private var session: NSURLSession?
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
@@ -21,6 +26,7 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        test()
     }
 
     override func didDeactivate() {
@@ -28,4 +34,42 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    func test() {
+        self.aqi = NSUserDefaults.standardUserDefaults().integerForKey("a")
+        self.concentration = NSUserDefaults.standardUserDefaults().doubleForKey("c")
+        self.time = NSUserDefaults.standardUserDefaults().stringForKey("t")
+        if self.time == nil {
+            self.time = "Invalid"
+        }
+        if self.aqi <= 1 {
+            self.aqi = -1
+        }
+        if self.concentration <= 1.0 {
+            self.concentration = -1.0
+        }
+        let request = createRequest()
+        if session == nil {
+            session = sessionForWatchExtension()
+        }
+        httpGet(session, request: request){
+            (data, error) -> Void in
+            if error != nil {
+                print(error)
+            } else {
+                let tmpAQI = parseAQI(data)
+                let tmpConcentration = parseConcentration(data)
+                let tmpTime = parseTime(data)
+                if tmpAQI > 1 && tmpConcentration > 1.0 && (tmpAQI != self.aqi || tmpConcentration != self.concentration || tmpTime != self.time) {
+                    self.aqi = tmpAQI
+                    self.concentration = tmpConcentration
+                    self.time = tmpTime
+                    print("wc - data loaded (by Interface Controller): api = \(self.aqi), concentration = \(self.concentration)， time = \(self.time)")
+                    let delegate = WKExtension.sharedExtension().delegate as! ExtensionDelegate
+                    delegate.wcUserInfo = ["a": self.aqi, "c": self.concentration, "t": self.time!]
+                    delegate.reloadComplication()
+                    return
+                }
+            }
+        }
+    }
 }
