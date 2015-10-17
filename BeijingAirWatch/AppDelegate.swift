1ae1cf93e612f018aab67dc91ed6f3daef80e9c6
Di Wu <diwufet@163.com>
2015-10-17 10:40:09 +0800

    add voip mode for background execution

--- BeijingAirWatch/AppDelegate.swift
+++ BeijingAirWatch/AppDelegate.swift
@@ -126,6 +126,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        UIApplication.sharedApplication().setKeepAliveTimeout(600) { () -> Void in
+            NSLog("called...")
+        }
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
